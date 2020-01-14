c12e450ae4a166db2db8b263b7fc8ad4b89dbd8e
banzay <yuriy.zdyrko@gmail.com>
2020-01-14 09:55:29 +0200

    2

---
+++ practice/lib/module.ex
@@ -0,0 +1,9 @@
+defmodule Times do
+    def times(n) do
+        n * 2
+    end
+
+    def triples(n), do: n * 3
+
+    def quadruple(n), do: times(n) * times(n)
+end
---
+++ practice/lib/number_guess.ex
@@ -0,0 +1,36 @@
+defmodule NumberGuess do
+    def f(actual, range \\ 1..100000) do
+        first = List.first(Enum.to_list(range))
+        last = List.last(Enum.to_list(range))
+    
+        check_match(actual, first, last)
+    end
+
+    
+    def check_match(actual, first, last) do
+        check_match(actual, first, last, middle(first, last))
+    end
+
+    def check_match(actual, first, _, middle) when actual < middle do
+        IO.inspect("--")
+        IO.inspect(first)
+        IO.inspect(middle)
+        IO.inspect("<")
+        check_match(actual, first, middle)
+    end
+    def check_match(actual, first, last, middle) when actual > middle do
+        IO.inspect("--")
+        IO.inspect(last)
+        IO.inspect(middle)
+        IO.inspect(">")
+        check_match(actual, middle, last)
+    end
+    def check_match(actual, first, _, middle) when actual == middle do
+        IO.inspect("SUCCESS")
+        actual
+    end
+
+    defp middle(first, last) do
+        first + Float.round(Kernel./ (last - first),  2)
+    end
+end
