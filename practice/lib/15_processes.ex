140f406a8fc0c0788baf7576a20f7ea190149bb0
zdyrko <yuriy.zdyrko@gmail.com>
2020-01-17 14:42:04 +0200

    page 344

--- practice/lib/15_processes.ex
+++ practice/lib/15_processes.ex
@@ -68,7 +68,6 @@ defmodule ProcessesChain do
     def child_process(next_pid) do
         receive do
             n ->
-                Process.sleep(10)
                 IO.inspect("In child process " <> to_string n)
                 send next_pid, n + 1
         end
@@ -102,5 +101,70 @@ defmodule ProcessesChain do
             n ->
                 IO.puts "In parent process " <> to_string n
         end
+    end
+end
+
+defmodule SpawnLink do
+    def failing_child do
+        Process.sleep(400)
+        exit(:boom)
+    end
+
+    def run do
+        # If we trap exit, failing_child process is not crashing parent,
+        # but sending {:EXIT, failing_child_pid, reason} to it.
+
+        Process.flag :trap_exit, true
+
+        spawn_link(__MODULE__, :failing_child, [])
+
+        receive do
+            {:EXIT, _pid, reason} ->
+                IO.inspect(reason)
+            anything ->
+                IO.inspect "WHY?"
+        end
+    end
+end
+
+defmodule SpawnMonitor do
+    def failing_child do
+        Process.sleep(400)
+        exit(:boom)
+    end
+
+    def run do
+        {pid, ref} = spawn_monitor(__MODULE__, :failing_child, [])
+
+        receive do
+            {:DOWN, _ref, :process, _pid, reason} ->
+                IO.inspect(reason)
+            anything ->
+                IO.inspect "WHY?"
+        end
+    end
+end
+
+
+defmodule Ex1 do
+    def child(parent_pid) do
+        send(parent_pid, :ping)
+    end
+
+    def run do
+        spawn_link(__MODULE__, :child, [self()])
+
+        Process.sleep 500
+
+        # look at mailbox
+        :erlang.process_info(self(), :messages) |> IO.inspect
+
+        receive do
+            msg ->
+                IO.inspect msg
+        end
+
+        # look at mailbox
+        :erlang.process_info(self(), :messages) |> IO.inspect
     end
 end
